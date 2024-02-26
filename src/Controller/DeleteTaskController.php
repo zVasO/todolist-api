23487941f58d7e5689385692adb19a3b44ede728
Dylan Germann <dev.dyger@gmail.com>
2024-02-26 17:04:57 +0100

    feat(controller): create the controller for delete a task

--- src/Controller/DeleteTaskController.php
+++ src/Controller/DeleteTaskController.php
@@ -3,27 +3,29 @@
 namespace App\Controller;
 
 use App\Command\CreateTaskCommand;
+use App\Command\DeleteTaskCommand;
 use App\Handler\CreateTaskHandler;
+use App\Handler\DeleteTaskHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/api/task', name: 'create_task', methods: ['POST'], format: 'json')]
-class CreateTaskController extends AbstractController
+#[Route('/api/task/{id}', name: 'delete_task', methods: ['DELETE'], format: 'json')]
+class DeleteTaskController extends AbstractController
 {
     public function __construct(
-        private CreateTaskHandler $handler,
+        private DeleteTaskHandler $handler,
     ) {
     }
 
-    public function __invoke(#[MapRequestPayload] CreateTaskCommand $command): Response
+    public function __invoke(int $id): Response
     {
+        $command = new DeleteTaskCommand($id);
         $user = $this->getUser();
-        $command->user = $user;
-        $this->handler->handle($command);
+        $this->handler->handle($command, $user);
 
-        return new Response(status: Response::HTTP_CREATED);
+        return new Response(status: Response::HTTP_NO_CONTENT);
     }
 }
